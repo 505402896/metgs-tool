6361fe6f3608437f312729fbce27a834aa8f5b11
Yuuki <505402896@qq.com>
2023-04-07 21:57:50 +0800

    chore: set resolve.alias in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@packages': resolve(__dirname, './packages')
+    }
+  },
   build: {
     lib: {
       entry: './packages/index.ts',
